31229f3fd6209137b1a3e8dbaca72eca05a13940
Boris Marion-Dorier <borismariondorier@gmail.com>
2022-10-26 13:16:43 +0200

    merged 'staging'

--- back-end/sogeti-technical-test/app/models/user.rb
+++ back-end/sogeti-technical-test/app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  before_validation :set_provider
+  before_validation :set_uid
+
+  def set_provider
+    self[:provider] = "email" if self[:provider].blank?
+  end
+
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
 end
