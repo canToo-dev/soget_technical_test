2201d5e99be3f4fb4867fce008bf792b6d380ad4
Boris Marion-Dorier <borismariondorier@gmail.com>
2022-10-26 18:51:28 +0200

    todo privacy done

--- back-end/sogeti-technical-test/app/models/user.rb
+++ back-end/sogeti-technical-test/app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :todos
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -16,4 +17,5 @@ class User < ActiveRecord::Base
   def set_uid
     self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
   end
+
 end
